a589a85348542764ff08f4d121003134f3f6c867
Medina <anyonegirl6@gmail.com>
2021-02-05 09:47:45 +0200

    Edit fact function added

--- router/factsRouter.js
+++ router/factsRouter.js
@@ -2,19 +2,20 @@ const express = require('express');
 const router = express.Router();
 const Fact = require('../models/createFact')
 
+//FACTS Home
 router.get('/', async (req, res) => {
     try {
         let facts = await Fact.find({}).sort({ createdAt: 'desc' }).lean();
-        console.log (facts)
-        res.render('facts', {facts:facts});
+        res.render('facts', { facts: facts });
     } catch{
         res
-        .status(400, 'A required action was not successful! Try again!')
-        .redirect('/teeth');
+            .status(400, 'A required action was not successful! Try again!')
+            .redirect('/teeth');
     }
 
 });
 
+//Create fact
 router.get('/create-fact', (req, res) => {
     res.render('create-fact');
 });
@@ -32,6 +33,35 @@ router.post('/create-fact', async (req, res) => {
     }
 });
 
+//Edit fact
+router.get('/edit/:id', async (req, res) => {
+    let id = req.params.id;
+    console.log(id)
+    try {
+        let fact = await Fact.findById(req.params.id).lean();
+        res.render('edit-fact', { fact })
+    } catch {
+        res
+            .status(400, 'A required action was not successful! Try again!')
+            .redirect('/teeth/facts');
+    }
+});
+
+router.put('/edit/:id', async (req, res) => {
+    try {
+        req.fact = await Fact.findById(req.params.id);
+        req.fact.creator = req.body.creator;
+        req.fact.content = req.body.content;
+        await req.fact.save();
+        res.redirect('/teeth/facts');
+    } catch  {
+        res
+            .status(400, 'A required action was not successful! Try again!')
+            .redirect('/teeth/facts');
+    }
+});
+
+//Delete fact
 router.delete('/:id', async (req, res) => {
     await Fact.findByIdAndDelete(req.params.id);
     res.redirect('/teeth/facts');
