f1e66320106f0a50c96cb060a5fab853c259e777
Medina <anyonegirl6@gmail.com>
2021-02-25 16:10:20 +0200

    added e-shop home page

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const app = require ('express').Router();
 const authRoute = require ('./router/auth')
 const personalRoute = require('./router/personal');
 const teethRoute = require('./router/teeth');
+const shopRoute = require ('./router/e-shop');
 
 const auth = require ('./middlewares/auth');
 const isAuthenticated = require ('./middlewares/isAuthenticated');
@@ -17,5 +18,7 @@ app.use ('/auth', authRoute);
 app.use('/personal',isAuthenticated, personalRoute);
 
 app.use('/teeth', teethRoute);
+
+app.use ('/e-shop', shopRoute);
 
 module.exports = app;
